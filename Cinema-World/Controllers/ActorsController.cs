ddd6e065281f15e80b8b1559c1d6ee172bc32e2d
Gunars Leitans <92DPGLeitans@rvt.lv>
2022-11-08 23:21:33 +0200

    Created controllers for Actors, Directors, Writers and Cinematography

---
+++ Cinema-World/Controllers/ActorsController.cs
@@ -0,0 +1,21 @@
+﻿using Cinema_World.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cinema_World.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ActorsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var allActors = _context.Actors.ToList();
+            return View();
+        }
+    }
+}
---
+++ Cinema-World/Controllers/DirectorsController.cs
@@ -0,0 +1,22 @@
+﻿using Cinema_World.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema_World.Controllers
+{
+    public class DirectorsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DirectorsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _context.Directors.ToListAsync();
+            return View();
+        }
+    }
+}
